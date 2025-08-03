ca54d91e8eec74f560ce2595e3bf75b38974bf25
riobotpair <dena74137@gmail.com>
2025-08-03 17:44:22 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'dena74137@gmail.com',
+    password: '12345Denushaa',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
